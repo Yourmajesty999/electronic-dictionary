6bf69aaf5166154d4a88b298c644ee1aebe9ada0
majesty <1510154383@qq.com>
2017-07-13 16:27:52 +0800

    electronic dictionary

---
+++ dictionary.js
@@ -0,0 +1,25 @@
+function getWords() {
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	  {// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	  }
+	else
+	  {// code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+
+	var words = document.getElementById('myWords').value;
+
+	xmlhttp.onreadystatechange=function()
+  	{
+  	if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    }
+  }
+	xmlhttp.open("GET","/ajax/demo_post2.asp",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("words");
+
+}
