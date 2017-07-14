7576ce8acbe2cf987221e1473978440c034bfe42
majesty <1510154383@qq.com>
2017-07-14 21:34:17 +0800

    finish the search page

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,4 @@
+
 function getWords() {
 	var xmlhttp;
 	if (window.XMLHttpRequest)
@@ -9,17 +10,68 @@ function getWords() {
 	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	  }
 
-	var words = document.getElementById('myWords').value;
-
+	
+	//var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function()
   	{
   	if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    var myObj = JSON.parse(this.responseText);
+    var content = ""
+    if(myObj.result && myObj.result.pronounces && myObj.result.translate && myObj.result.web_translate){
+	content += myObj.result.pronounces.join("<br>") + "<br>" + myObj.result.translate.join("<br>") + "<br>" + "网络释义：" + myObj.result.web_translate
     }
-  }
-	xmlhttp.open("GET","/ajax/demo_post2.asp",true);
-	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xmlhttp.send("words");
+    document.getElementById("myDiv").innerHTML=content;
+    }
+  };
+  var words = document.getElementById('myWords').value;
+	xmlhttp.open("GET","/dictionary/eng/" + words);
+	//xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send();
 
 }
+
+function do_getword(e){
+	if(e.code === 'Enter'){
+		getWords()
+	}
+}
+/*
+function getWords(){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	        var myObj = JSON.parse(this.responseText);
+	        var content = ""
+	        if(myObj.result && myObj.result.translate){
+	        	content += myObj.result.translate.join("<br>")
+	        }
+	        document.getElementById("myDiv").innerHTML = content;
+	    }
+	};
+	var words = document.getElementById('myWords').value;
+	xmlhttp.open("GET", "/dictionary/eng/" + words);
+	xmlhttp.send();
+}
+*/
+
+function myCanvas() {
+	var c = document.getElementById("myCanvas");
+	var img = document.createElement("img");
+	img.src="wilde.jpg";
+	c.appendChild(img);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
